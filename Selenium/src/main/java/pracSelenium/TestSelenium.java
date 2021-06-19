d799370ac5392128f12928625b9b0b6e26c0359a
Marcial Rivera <marcial.rivera@autozone.com>
2021-06-19 11:19:07 -0500

    Innital Commit

---
+++ Selenium/src/main/java/pracSelenium/TestSelenium.java
@@ -0,0 +1,17 @@
+package pracSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String exePath = "./drivers/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+	}
+
+}
---
+++ Selenium/src/test/java/pruebaNG/NewTest.java
@@ -0,0 +1,11 @@
+package pruebaNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  Assert.assertTrue(false);
+  }
+}
